f986412f9a87e706524ddaa30f6b06b2d76b404f
zurachu <zurachu@gmail.com>
2021-04-30 00:05:35 +0900

    DRY

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -14,14 +14,14 @@ public static class Wave
 
     public static List<Tile.Info> AllNumbers(Tile.Info.Type type)
     {
-        return Enumerable.Range(1, 9).ToList().ConvertAll(_number => new Tile.Info(type, _number));
+        return Numbers().ConvertAll(_number => new Tile.Info(type, _number));
     }
 
     public static List<Tile.Info> RandomOneTypeTiles(int count, int targetCount)
     {
         var numbers = new List<int>(Enumerable.Repeat(Tile.Info.TargetNumber, targetCount).ToList());
 
-        var notTargetNumbers = Enumerable.Range(1, 9).ToList();
+        var notTargetNumbers = Numbers();
         notTargetNumbers.Remove(Tile.Info.TargetNumber);
         for (var i = targetCount; i < count; i++)
         {
@@ -37,5 +37,10 @@ public static class Wave
     {
         var types = new List<Tile.Info.Type> { Tile.Info.Type.Man, Tile.Info.Type.Pin, Tile.Info.Type.Sou };
         return ListUtility.Random(types);
+    }
+
+    private static List<int> Numbers()
+    {
+        return Enumerable.Range(1, 9).ToList();
     }
 }
