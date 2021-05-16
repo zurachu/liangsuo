fef792f559301982ead5c6c5d9969d04aa8aab4a
zurachu <zurachu@gmail.com>
2021-05-16 23:06:21 +0900

    UI調整

---
+++ Assets/Scripts/Utilities/WebUtility.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+public static class WebUtility
+{
+#if UNITY_WEBGL
+    // naichilab.UnityRoomTweet
+    [DllImport("__Internal")]
+    private static extern void OpenWindow(string url);
+#endif
+
+    public static void OpenURL(string url)
+    {
+#if UNITY_WEBGL
+        OpenWindow(url);
+#else
+        Application.OpenURL(url);
+#endif
+    }
+}
